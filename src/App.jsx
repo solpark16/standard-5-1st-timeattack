5b5fb61409a24ab6fdf072e84616261b2fb1b50e
Sol Park <solpark16@gmail.com>
2024-05-20 15:38:42 +0900

    calculator 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
+import { useState } from "react";
 export default function App() {
+  const [value, setValue] = useState(1);
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input
+          type="number"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />{" "}
+        만큼을{" "}
+        <button
+          onClick={() => {
+            setCount(count + +value);
+          }}
+        >
+          더할게요
+        </button>{" "}
+        <button
+          onClick={() => {
+            setCount(count - +value);
+          }}
+        >
+          뺄게요
+        </button>
+        <button
+          onClick={() => {
+            setCount(0);
+          }}
+        >
+          초기화
+        </button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{count}</p>
       </div>
     </div>
   );
